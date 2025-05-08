92358a2e62d2bab2c86be5164ce0750dd4d44198
2023JoshuaP <jphilco@unsa.edu.pe>
2025-05-08 10:14:50 -0500

    Codigo corregido para la rotacion antihoraria.

--- Unidad 1/Imagenes con OpenCV/Modify_Pixels_Rotation.cpp
+++ Unidad 1/Imagenes con OpenCV/Modify_Pixels_Rotation.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 using namespace cv;
 
-Mat rotate_image(const Mat& image) {
+Mat rotate_image_antihorary(const Mat& image) {
     int rows = image.rows;
     int columns = image.cols;
 
-    Mat image_rotate(rows, columns, image.type());
-
+    Mat image_rotate(columns, rows, CV_8UC3);
+    
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
-            image_rotate.at<Vec3b>(j, rows - i - 1) = image.at<Vec3b>(i, j);
+            image_rotate.at<Vec3b>(columns - 1 - j, i) = image.at<Vec3b>(i, j);
         }
     }
 
@@ -23,6 +23,9 @@ int main() {
     string image_file;
     cout << "Ingrese el nombre de la imagen (con extension): ";
     cin >> image_file;
+    cout << "Cuantas rotaciones desea hacer? (1-4): ";
+    cin >> number_of_rotations;
+
     string image_path = "D:/UNSA EPCC/7mo semestre/Computacion Grafica/Unidad 1/Imagenes con OpenCV/Imagenes/" + image_file;
 
     Mat image_original = imread(image_path);
@@ -33,20 +36,17 @@ int main() {
 
     imshow("Imagen Original", image_original);
 
-    cout << "Cuantas rotaciones desea hacer? (1-4): ";
-    cin >> number_of_rotations;
-
     if (number_of_rotations < 1 || number_of_rotations > 4) {
         cout << "Numero de rotaciones no valido." << endl;
         return -1;
     }
 
-    Mat image_rotated = image_original;
+    Mat image_rotated_antihorary = image_original;
     for (int i = 0; i < number_of_rotations; i++) {
-        image_rotated = rotate_image(image_rotated);
+        image_rotated_antihorary = rotate_image_antihorary(image_rotated_antihorary);
     }
 
-    imshow("Imagen Rotada", image_rotated);
+    imshow("Imagen Rotada Antihorario", image_rotated_antihorary);
 
     waitKey(0);
 
