eeb06b4e38d214a71319f602609678fe3803126f
2023JoshuaP <jphilco@unsa.edu.pe>
2025-05-17 23:36:01 -0500

    Agregando otra funcion que permita separar los canales para aplicar la transformacion del color para ver si hay diferencias.

--- Unidad 1/Imagenes con OpenCV/Transformation_Image.cpp
+++ Unidad 1/Imagenes con OpenCV/Transformation_Image.cpp
@@ -8,9 +8,15 @@ vector<Vec3b> color_map_personalized() {
     Mat color_map_image(50, 256, CV_8UC3);
     for (int i = 0; i < 256; i++) {
         float t = i /255.0f;
+        /* Primer Mapa de Color */
         uchar b = static_cast<uchar>(255 * (1 - t));
         uchar g = static_cast<uchar>(255*t);
         uchar r = 128;
+
+        /* Segundo Mapa de Color */
+        // uchar b = static_cast<uchar>(255 * (1 - t));
+        // uchar g = 64;
+        // uchar r = static_cast<uchar>(255*t);
         Vec3b c(b, g, r);
         color_map[i] = c;
         for (int j = 0; j < 50; j++) {
@@ -51,6 +57,43 @@ Mat create_image_personalized(const Mat& image, const vector<Vec3b>& color_map) 
     return image_transformed;
 }
 
+void apply_color_map_channels(const Mat& image, const vector<Vec3b>& color_map) {
+    int rows = image.rows;
+    int cols = image.cols;
+
+    Mat color_map_channel_red(rows, cols, CV_8UC3);
+    Mat color_map_channel_green(rows, cols, CV_8UC3);
+    Mat color_map_channel_blue(rows, cols, CV_8UC3);
+    Mat imaged_transformed(rows, cols, CV_8UC3);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            int channel_blue = image.at<Vec3b>(i, j)[0];
+            int channel_green = image.at<Vec3b>(i, j)[1];
+            int channel_red = image.at<Vec3b>(i, j)[2];
+
+            Vec3b color_value_blue = color_map[channel_blue];
+            Vec3b color_value_green = color_map[channel_green];
+            Vec3b color_value_red = color_map[channel_red];
+
+            color_map_channel_blue.at<Vec3b>(i, j) = color_value_blue;
+            color_map_channel_green.at<Vec3b>(i, j) = color_value_green;
+            color_map_channel_red.at<Vec3b>(i, j) = color_value_red;
+
+            Vec3b color_avg;
+            for (int k = 0; k < 3; k++) {
+                color_avg[k] = static_cast<uchar>((color_value_blue[k] + color_value_green[k] + color_value_red[k]) / 3);
+            }
+            imaged_transformed.at<Vec3b>(i, j) = color_avg;
+        }
+    }
+
+    imshow("Canal Azul con Mapa de Color", color_map_channel_blue);
+    imshow("Canal Verde con Mapa de Color", color_map_channel_green);
+    imshow("Canal Rojo con Mapa de Color", color_map_channel_red);
+    imshow("Imagen Transformada Unida", imaged_transformed);
+}
+
 int main() {
     string image_file;
     cout << "Ingrese el nombre de la imagen (con extension): ";
@@ -63,6 +106,7 @@ int main() {
     }
     vector<Vec3b> color_map = color_map_personalized();
     Mat image_transformed = create_image_personalized(image_original, color_map);
+    apply_color_map_channels(image_original, color_map);
     imshow("Imagen Original", image_original);
     imshow("Imagen Transformada", image_transformed);
     waitKey(0);
