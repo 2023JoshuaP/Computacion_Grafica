bfc4bfdecb6c890970b0d82c900cc97ee7f0c680
2023JoshuaP <jphilco@unsa.edu.pe>
2025-05-25 11:09:10 -0500

    Structuring the equalization function, correct operation.

--- Unidad 1/Imagenes con OpenCV/Histogram.cpp
+++ Unidad 1/Imagenes con OpenCV/Histogram.cpp
@@ -1,43 +1,75 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
 
-vector<int> calculate_histogram(const Mat& image) {
+vector<int> generate_histogram(const Mat& image) {
     vector<int> histogram(256, 0);
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
+    int rows = image.rows;
+    int columns = image.cols;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
             int pixel_value = image.at<uchar>(i, j);
             histogram[pixel_value]++;
         }
     }
-    return histogram;
+    return histogram;;
 }
 
 void save_histogram(const vector<int>& histogram, const string& filename) {
-    ofstream file_histogram(filename);
-    for (int i = 0; i < histogram.size(); i++) {
-        file_histogram << i << " " << histogram[i] << endl;
+    ofstream file(filename);
+    for (size_t i = 0; i < histogram.size(); i++) {
+        file << i << " " << histogram[i] << endl;
     }
-    file_histogram.close();
+    file.close();
+}
+
+Mat equalization(const Mat& image, const vector<int>& histogram) {
+    Mat image_equalized = image.clone();
+    int total_pixels = image.rows * image.cols;
+    vector<uchar> f(256);
+    
+    int acumulado = histogram[0];
+    f[0] = 0;
+    for (int i = 1; i < 255; i++) {
+        f[i] = (uchar)((255.0f * acumulado) / total_pixels);
+        acumulado += histogram[i];
+    }
+    f[255] = 255;
+    
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            uchar pixel_value = image.at<uchar>(i, j);
+            image_equalized.at<uchar>(i, j) = f[pixel_value];
+        }
+    }
+
+    return image_equalized;
 }
 
 int main() {
-    string file_name;
-    cout << "Poner imagen con extensión: ";
-    cin >> file_name;
-    string path_image = "Imagenes/" + file_name;
-
+    string image;
+    cout << "Enter the name file image (with extension): ";
+    cin >> image;
+    string path_image = "D:/UNSA EPCC/7mo semestre/Computacion Grafica/Unidad 1/Imagenes con OpenCV/Imagenes/Equalization Histogram Images/" + image;
+    string path_histogram = "D:/UNSA EPCC/7mo semestre/Computacion Grafica/Unidad 1/Imagenes con OpenCV/Imagenes/Equalization Histogram Images/";
     Mat image_original = imread(path_image, IMREAD_GRAYSCALE);
+    
     if (image_original.empty()) {
-        cout << "No se pudo abrir la imagen." << endl;
+        cerr << "Error loading image: " << path_image << endl;
         return -1;
     }
 
-    imshow("Imagen Original", image_original);
+    imshow("Image Original", image_original);
 
-    vector<int> histogram = calculate_histogram(image_original);
-    save_histogram(histogram, "histograma.txt");
+    vector<int> histogram = generate_histogram(image_original);
+    save_histogram(histogram, path_histogram + "histogram.txt");
+
+    Mat image_equalized = equalization(image_original, histogram);
+    imshow("Image Equalized", image_equalized);
+
     waitKey(0);
     return 0;
 }
