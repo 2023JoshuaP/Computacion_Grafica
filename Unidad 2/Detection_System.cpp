40597658a1249e54083b9c6e6165abf500a85bef
2023JoshuaP <jphilco@unsa.edu.pe>
2025-06-03 11:47:34 -0500

    Transforming the image into binarized grayscale for better results.

--- Unidad 2/Detection_System.cpp
+++ Unidad 2/Detection_System.cpp
@@ -3,20 +3,47 @@
 using namespace std;
 using namespace cv;
 
-// Mat process_erosion(const Mat& src, int kernel_size = 3) {}
+Mat binarized(const Mat& image, int threshold) {
+    Mat image_binarized = image.clone();
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            uchar pixel_value = image.at<uchar>(i, j);
+            if (pixel_value > threshold) {
+                image_binarized.at<uchar>(i, j) = 255;
+            }
+            else {
+                image_binarized.at<uchar>(i, j) = 0;
+            }
+        }
+    }
+    return image_binarized;
+}
 
-// Mat process_dilatation(const Mat& src, int kernel_size = 3) {}
+Mat gray_scale(const Mat& image) {
+    Mat gray_image(image.rows, image.cols, CV_8UC1);
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            Vec3b pixel = image.at<Vec3b>(i, j);
+            uchar gray_value = static_cast<uchar>(0.21 * pixel[2] + 0.72 * pixel[1] + 0.07 * pixel[0]);
+            gray_image.at<uchar>(i, j) = gray_value;
+        }
+    }
+    return gray_image;
+}
 
 int main() {
     VideoCapture cap(0);
-    Mat frame;
+    Mat frame, gray, binarized_frame;
+    int threshold = 128;
     while (true) {
         cap >> frame;
         if (frame.empty()) {
             cerr << "Error al capturar el frame.\n";
             break;
         }
-        imshow("Webcam", frame);
+        gray = gray_scale(frame);
+        binarized_frame = binarized(gray, threshold);
+        imshow("Webcam", binarized_frame);
         if (waitKey(1) == 'q') {
             break;
         }
