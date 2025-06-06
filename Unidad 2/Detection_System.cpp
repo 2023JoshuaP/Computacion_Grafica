0a1be922f56f933d44d9525f160114bc174b7144
2023JoshuaP <jphilco@unsa.edu.pe>
2025-06-06 12:33:08 -0500

    Testing object recognition, try a video first.

--- Unidad 2/Detection_System.cpp
+++ Unidad 2/Detection_System.cpp
@@ -56,12 +56,50 @@ int main() {
         frame_binarized = binarized_frame(frame_gray, (int)threshold_value);
 
         // imshow("Frame Capture", frame_capture);
-        imshow("Frame Gray", frame_gray);
+        // imshow("Frame Gray", frame_gray);
         // imshow("Frame Binarized", frame_binarized);
+
+        vector<vector<Point>> contours;
+        findContours(frame_binarized, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        Point center;
+
+        if (!contours.empty()) {
+            double max_area = 0;
+            int max_index = -1;
+            for (size_t i = 0; i < contours.size(); i++) {
+                double area = contourArea(contours[i]);
+                if (area > max_area) {
+                    max_area = area;
+                    max_index = i;
+                }
+            }
+            if (max_index >= 0) {
+                Moments m = moments(contours[max_index]);
+                if (m.m00 != 0) {
+                    center = Point(m.m10 / m.m00, m.m01 / m.m00);
+                    Mat mask(frame_gray.rows + 2, frame_gray.cols + 2, CV_8UC1, Scalar(0));
+                    floodFill(frame_binarized, mask, center, Scalar(255), 0, Scalar(5), Scalar(5), FLOODFILL_MASK_ONLY | 4);
+                    Mat mask_roi = mask(Rect(1, 1, frame_gray.cols, frame_gray.rows));
+                    Moments fm = moments(mask_roi, true);
+                    if (fm.m00 != 0) {
+                        Point flood_center = Point(fm.m10 / fm.m00, fm.m01 / fm.m00);
+                        trajectory.push_back(flood_center);
+                        circle(frame_capture, flood_center, 5, Scalar(0, 255, 0), -1);
+                    }
+                }
+            }
+        }
+
         if (waitKey(1) == 'q') {
             cout << "Exiting..." << endl;
             break;
         }
+
+        for (size_t i = 1; i < trajectory.size(); i++) {
+            line(frame_capture, trajectory[i - 1], trajectory[i], Scalar(0, 0, 255), 2);
+        }
+
+        imshow("Trajectory", frame_capture);
     }
 
     cap.release();
