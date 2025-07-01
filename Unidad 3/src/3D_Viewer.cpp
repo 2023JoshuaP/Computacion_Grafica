a96e62a7cdf8810e2323d3a6ab8b69bcc950f469
2023JoshuaP <jphilco@unsa.edu.pe>
2025-06-30 20:05:38 -0500

    Molecule viewer completed, improvements later.

--- Unidad 3/src/3D_Viewer.cpp
+++ Unidad 3/src/3D_Viewer.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 using namespace glm;
 
+/* Shaders a utilizar (codigo GLSL almacenado en un string ejecutada en el GPU) */
 const char* vertex_shader_source = R"(
 #version 330 core
 layout (location = 0) in vec3 aPos;
@@ -27,6 +28,7 @@ void main() {
     FragColor = vec4(color, 1.0);
 })";
 
+/* Estructuras a utilizar en el visor */
 struct Atom {
     float position_x, position_y, position_z;
     string symbol;
@@ -36,11 +38,13 @@ struct Bond {
     int atom_1, atom_2;
 };
 
+/* Variables globales a utilizar */
 vector<Atom> atoms;
 vector<Bond> bonds;
 float rotation_x = 0, rotation_y = 0, zoom = 10.0f;
 const float rotation_speed = 0.1f, zoom_speed = 0.02f;
 
+/* Funcion para cargar y leer el archivo de moleculas */
 void load_file(const string& file) {
     ifstream file_molecule(file);
     if (!file_molecule.is_open()) {
@@ -73,6 +77,7 @@ void load_file(const string& file) {
     }
 }
 
+/* Funcion para crear shaders basicos */
 unsigned int create_shader() {
     unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
@@ -92,6 +97,7 @@ unsigned int create_shader() {
     return program;
 }
 
+/* Funcion para interaccion del usuario al usar el visor */
 void interface_user(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
@@ -109,14 +115,83 @@ void interface_user(GLFWwindow* window) {
         zoom += zoom_speed;
 }
 
+/* Funcion de inicializacion para el buffer de Atomos */
+void atom_buffer(unsigned int& VaO, unsigned int& VbO, const vector<Atom>& atoms) {
+    vector<float> vertices;
+    for (const auto& atom : atoms) {
+        vertices.push_back(atom.position_x);
+        vertices.push_back(atom.position_y);
+        vertices.push_back(atom.position_z);
+    }
+    glGenVertexArrays(1, &VaO);
+    glGenBuffers(1, &VbO);
+    glBindVertexArray(VaO);
+    glBindBuffer(GL_ARRAY_BUFFER, VbO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+/* Funcion de inicializacion para el buffer de Vinculos */
+void bond_buffer(unsigned int& VaO, unsigned int& VbO, const vector<Bond>& bonds, const vector<Atom>& atoms) {
+    vector<float> vertices;
+    for (const auto& bond : bonds) {
+        if (bond.atom_1 < atoms.size() && bond.atom_2 < atoms.size()) {
+            const Atom& a1 = atoms[bond.atom_1];
+            const Atom& a2 = atoms[bond.atom_2];
+            vertices.push_back(a1.position_x);
+            vertices.push_back(a1.position_y);
+            vertices.push_back(a1.position_z);
+            vertices.push_back(a2.position_x);
+            vertices.push_back(a2.position_y);
+            vertices.push_back(a2.position_z);
+        }
+    }
+    glGenVertexArrays(1, &VaO);
+    glGenBuffers(1, &VbO);
+    glBindVertexArray(VaO);
+    glBindBuffer(GL_ARRAY_BUFFER, VbO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+/* Funcion que muestra la molecula en el visor 3D */
+void render(GLFWwindow* window, unsigned int shader, unsigned int atom_VaO, unsigned int bond_VaO, const vector<Atom>& atoms, const vector<Bond>& bonds) {
+    glPointSize(8.0f);
+    while (!glfwWindowShouldClose(window)) {
+        interface_user(window);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader);
+
+        mat4 model = mat4(1.0f);
+        model = rotate(model, radians(rotation_x), vec3(1.0f, 0.0f, 0.0f));
+        model = rotate(model, radians(rotation_y), vec3(0.0f, 1.0f, 0.0f));
+        mat4 view = translate(mat4(1.0f), vec3(0.0f, 0.0f, -zoom));
+        mat4 projection = perspective(radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        mat4 mvp = projection * view * model;
+
+        int mvp_location = glGetUniformLocation(shader, "mvp");
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, &mvp[0][0]);
+        int color_location = glGetUniformLocation(shader, "color");
+        glUniform3f(color_location, 1.0f, 0.2f, 0.2f);
+        glBindVertexArray(atom_VaO);
+        glDrawArrays(GL_POINTS, 0, atoms.size());
+        glUniform3f(color_location, 1.0f, 1.0f, 1.0f);
+        glBindVertexArray(bond_VaO);
+        glDrawArrays(GL_LINES, 0, bonds.size() * 2);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
 int main() {
     string file_molecule = "D:/UNSA EPCC/7mo semestre/Computacion Grafica/Unidad 3/Conformer3D_COMPOUND_CID_2244.sdf";
-
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
     GLFWwindow* window_view = glfwCreateWindow(800, 600, "Molecule Viewer", NULL, NULL);
     if (!window_view) {
         cerr << "Failed to create GLFW window" << endl;
@@ -127,76 +202,14 @@ int main() {
     gladLoadGL();
     glad_glViewport(0, 0, 800, 600);
     glEnable(GL_DEPTH_TEST);
-
+    
     load_file(file_molecule);
     unsigned int shader_program = create_shader();
 
-    vector<float> atom_vertices;
-    for (const auto& atom : atoms) {
-        atom_vertices.push_back(atom.position_x);
-        atom_vertices.push_back(atom.position_y);
-        atom_vertices.push_back(atom.position_z);
-    }
-
-    vector<float> bond_vertices;
-    for (const auto& bond : bonds) {
-        if (bond.atom_1 < atoms.size() && bond.atom_2 < atoms.size()) {
-            bond_vertices.push_back(atoms[bond.atom_1].position_x);
-            bond_vertices.push_back(atoms[bond.atom_1].position_y);
-            bond_vertices.push_back(atoms[bond.atom_1].position_z);
-            
-            bond_vertices.push_back(atoms[bond.atom_2].position_x);
-            bond_vertices.push_back(atoms[bond.atom_2].position_y);
-            bond_vertices.push_back(atoms[bond.atom_2].position_z);
-        }
-    }
-
-    unsigned int atom_VBO, atom_VAO;
-    glGenVertexArrays(1, &atom_VAO);
-    glGenBuffers(1, &atom_VBO);
-    glBindVertexArray(atom_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, atom_VBO);
-    glBufferData(GL_ARRAY_BUFFER, atom_vertices.size() * sizeof(float), atom_vertices.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    unsigned int bond_VBO, bond_VAO;
-    glGenVertexArrays(1, &bond_VAO);
-    glGenBuffers(1, &bond_VBO);
-    glBindVertexArray(bond_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, bond_VBO);
-    glBufferData(GL_ARRAY_BUFFER, bond_vertices.size() * sizeof(float), bond_vertices.data(), GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glPointSize(8.0f);
-
-    while (!glfwWindowShouldClose(window_view)) {
-        interface_user(window_view);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(shader_program);
-        mat4 model = mat4(1.0f);
-        model = rotate(model, radians(rotation_x), vec3(1.0f, 0.0f, 0.0f));
-        model = rotate(model, radians(rotation_y), vec3(0.0f, 1.0f, 0.0f));
-        mat4 view = translate(mat4(1.0f), vec3(0.0f, 0.0f, -zoom));
-        mat4 projection = perspective(radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-        mat4 mvp = projection * view * model;
-
-        int mvp_localitation = glGetUniformLocation(shader_program, "mvp");
-        glUniformMatrix4fv(mvp_localitation, 1, GL_FALSE, &mvp[0][0]);
-
-        int color_location = glGetUniformLocation(shader_program, "color");
-        glUniform3f(color_location, 1.0f, 0.2f, 0.2f);
-
-        glBindVertexArray(atom_VAO);
-        glDrawArrays(GL_POINTS, 0, atoms.size());
-        glUniform3f(color_location, 1.0f, 1.0f, 1.0f);
-        glBindVertexArray(bond_VAO);
-        glDrawArrays(GL_LINES, 0, bond_vertices.size() / 3);
-        glfwSwapBuffers(window_view);
-        glfwPollEvents();
-    }
+    unsigned int atom_VAO, atom_VBO, bond_VAO, bond_VBO;
+    atom_buffer(atom_VAO, atom_VBO, atoms);
+    bond_buffer(bond_VAO, bond_VBO, bonds, atoms);
+    render(window_view, shader_program, atom_VAO, bond_VAO, atoms, bonds);
 
     glDeleteVertexArrays(1, &atom_VAO);
     glDeleteBuffers(1, &atom_VBO);
